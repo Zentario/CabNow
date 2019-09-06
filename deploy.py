44c1939d74a88afe865b4e952dbb6ec36a557347
G Anirudh <31547670+Zentario@users.noreply.github.com>
2019-09-07 00:28:59 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -165,5 +165,5 @@ def loginUser():
 
 
 #For testing only
-if __name__ == "__main__":
-   app.run(host="127.0.0.1", port=5000, debug=True)
+#if __name__ == "__main__":
+#   app.run(host="127.0.0.1", port=5000, debug=True)
